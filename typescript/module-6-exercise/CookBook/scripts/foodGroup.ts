5c6f7a063ea391004b4477ccd9ae519a9735fb49
Craig Doremus <cdoremus@gmail.com>
2015-04-22 11:00:07 -0400

    Initial commit of Module 4,5 & 6 exercises

---
+++ typescript/module-6-exercise/CookBook/scripts/foodGroup.ts
@@ -0,0 +1,16 @@
+﻿class FoodGroup {
+    name: string;
+
+    constructor(name: string) {
+        //TODO
+        //Assign the "name" parameter to the 
+        //"name" member variable 
+        this.name = name;
+
+    }  
+
+    //Added for debugging
+    public toString() {
+    	return 'FoodGroup(name:' + this.name + ')';
+    }
+} 
