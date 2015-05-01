6f23dd5d1cf69f37a255f61b08b6e48f537c0a55
Craig Doremus <cdoremus@gmail.com>
2015-04-30 23:29:54 -0400

    Fixed lines 41 & 42 instantiation of recipeCategorySummary and addint to the recipeCategoriesSummary items array

--- typescript/module-9-exercise/CookBook/scripts/recipeLoader.ts
+++ typescript/module-9-exercise/CookBook/scripts/recipeLoader.ts
@@ -15,12 +15,13 @@ class RecipeLoader {
             //TODO
             //Change RecipeCategories to use the new generic type.
             //Pass IRecipeCategory as the type
-            recipeCategories = new RecipeCategories();
+            recipeCategories = new RecipeCategories<IRecipeCategory>();
 
             //TODO
             //Create a new RecipeCategories object named recipeCategoriesSummary
             //and pass an IRecipeCategorySummary as the generic value.
-            
+            var recipeCategoriesSummary = new RecipeCategories<IRecipeCategorySummary>();
+
             categories.forEach((category) => {
                 var recipeCategory = new RecipeCategory({
                     name: category.title,
@@ -37,6 +38,8 @@ class RecipeLoader {
                 //object's items collection
                 //HINT: The constructor object passed must match the IRecipeCategorySummary interface
                 //HINT: Use the push() function
+                var recipeCategorySummary = new RecipeCategorySummary({text: category.title, title: category.details});
+                recipeCategoriesSummary.items.push(recipeCategorySummary);
                 
             });
             
