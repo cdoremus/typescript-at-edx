8caab617dad7e7b7447faa0c0ae5448253495764
Craig Doremus <cdoremus@gmail.com>
2015-05-02 11:10:50 -0400

    Copeid from module-9, creating a RecipeData module in interfaces.ts with all interfaces in the module exported. Fixed references throughout.

---
+++ typescript/module-10-exercise/CookBook/scripts/recipeCategory.ts
@@ -0,0 +1,14 @@
+﻿//FROM MODULE 7&8:
+//Fill out the body of this class so it extends BaseRecipeCategory
+//and implements IRecipeCategory
+class RecipeCategory  extends BaseRecipeCategory implements RecipeData.IRecipeCategory {
+	description: string;
+	examples: IExample[];
+
+   constructor(recipeCategory: RecipeCategory) {
+        super(recipeCategory.name, recipeCategory.foodGroups);
+        this.description = recipeCategory.description;
+        this.examples = recipeCategory.examples;
+    }
+
+} 
---
+++ typescript/module-10-exercise/CookBook/scripts/recipeCategorySummary.ts
@@ -0,0 +1,17 @@
+﻿//TODO
+//Modify the class below named RecipeCategorySummary to do the following:
+//1. Implements the IRecipeCategorySummary
+//2. Defines two member variables:
+//   text - string
+//   title - string
+//3. Create a constructor that accepts a single parameter
+//   of type IRecipeCategorySummary
+//4. Assign the values from the constructor parameter to the member variables
+class RecipeCategorySummary  implements RecipeData.IRecipeCategorySummary {
+    text: string;
+    title: string;
+    constructor(recipeCategorySummary: RecipeData.IRecipeCategorySummary) {
+    	this.text = recipeCategorySummary.text;
+    	this.title = recipeCategorySummary.title;
+    }
+}
